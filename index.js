1dbd53ab7c964ef823f9cea4d3a32234fa3b7d50
arunsybex <arunkumar@chainflux.com>
2020-12-18 15:39:32 +0530

    dasda

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const tezosServerUrl = 'https://tezos-dev.cryptonomic-infra.tech:443';
+const conseilServerConfig = { url: 'https://conseil-dev.cryptonomic-infra.tech:443', apiKey: 'hooman', network: 'delphinet' } as ConseilServerInfo;
+const uteContractAddress = 'KT1TVMrbibvGTxHZ7ttCDFAx3XGoh2zp2iDQ';
+
 getNewWallet = async() => {
     return {"status":true,"data":await conseiljssoftsigner.KeyStoreUtils.generateIdentity(),"key":"getNewWallet"}
+}
+
+getUteBalance = async(publicKeyHash) => {
+    const storage = await conseiljs.Tzip7ReferenceTokenHelper.getSimpleStorage(tezosServerUrl, uteContractAddress);
+    const mapId = storage.mapid;
+    let balance = 0.00;
+    try {
+        balance = await conseiljs.Tzip7ReferenceTokenHelper.getAccountBalance(tezosServerUrl, mapId, publicKeyHash);
+    }
+    catch (e) {
+        logger.error('Unable to fetch UTE balance', e);
+        return undefined;
+    }
+    return {"status":true,"data":(balance * 1.0) / 1000000.00,"key":"getUteBalance"}
 }
